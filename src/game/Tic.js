0a3aefaf76d937215085ded0e44c174397d93acf
yuensl <42883080+yuensl@users.noreply.github.com>
2021-03-22 18:13:49 +0800

    update

--- src/game/Tic.js
+++ src/game/Tic.js
@@ -29,9 +29,21 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
     //return <Square value={i} />;
-    return <Square value={this.state.squares[i]} />;
+    //return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
